052d7984b70875be0c8f4986983216488a972b43
YoungBom <p01039974051@gmail.com>
2025-07-29 16:27:34 +0900

    7월 29일자 공부한것.
    인터페이스~
    타입변환과 다형성

---
+++ chap08/src/sec01/exam04/quiz/Actionable.java
@@ -0,0 +1,8 @@
+package sec01.exam04.quiz;
+
+public interface Actionable {
+
+//	(public abstract) void action(); // 앞에 인터페이스 안에서는 이렇게 안써도 알아서 붙는다.
+	 void action();
+
+}
---
+++ chap08/src/sec01/exam06/MyClass.java
@@ -0,0 +1,36 @@
+package sec01.exam06;
+
+public class MyClass {
+	// 필드
+	// 1. 필드의 타입
+	RemoteControl rc = new Television();
+
+	// 2. 생성자 매게 변수의 타입으로 사용
+	public MyClass() {
+
+	}
+
+	public MyClass(RemoteControl rc) {
+		this.rc = rc;
+		rc.turnOn();
+		rc.turnOff();
+		rc.setVolume(5);
+
+	}
+
+	// 메소드
+	void methodA() {
+		// 3. 로컬 변수의 타입으로 사용
+		RemoteControl rc = new Audio();
+		rc.turnOn();
+		rc.setVolume(5);
+
+	}
+
+	// 4. 메소드 매게 변수의 타입으로 사용
+	void methodB(RemoteControl rc) {
+		rc.turnOn();
+		rc.setVolume(5);
+	}
+
+}
